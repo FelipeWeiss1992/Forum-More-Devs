1c3c440c03c906be4100ec557e3326e6ba2cc97b
Felipe Weiss <felipe.weiss@proway.treina>
2023-01-25 19:57:47 -0300

    atualizando forum

--- models/forms.py
+++ models/forms.py
@@ -1,9 +1,7 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import data_required, Length, Email, EqualTo, ValidationError
+from wtforms.validators import data_required, EqualTo
 from models.forms import *
-from flask_login import current_user
 
 class FormCriarSubPost(FlaskForm):
     description = TextAreaField('', validators=[data_required()])
@@ -14,7 +12,6 @@ class FormCriarPost(FlaskForm):
     title = StringField('Titulo', validators=[data_required()])
     description = TextAreaField('Post', validators=[data_required()])
     botao_submit_criarpost = SubmitField('Criar Post')
-
 
 
 class FormCriarConta(FlaskForm):
@@ -32,13 +29,4 @@ class FormLogin(FlaskForm):
     botao_submit_login = SubmitField('Login')
 
 
-class FormEditarPerfil(FlaskForm):
-    username = StringField('Nome de Usuário', validators=[data_required()])
-    email = StringField('E-mail', validators=[data_required(), Email()])
-    foto_perfil = FileField('Atualizar Foto de Perfil', validators=[FileAllowed(['jpg', 'png'])])
-    java = BooleanField('Java')
-    python = BooleanField('Python')
-    html = BooleanField('HTML')
-    javascript = BooleanField('JavaScript')
-    sql = BooleanField('SQL')
-    botao_submit_editarperfil = SubmitField('Confirmar Edição')
+
